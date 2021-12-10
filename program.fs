0442f5c43248cf2fc13938fc324bc2e2914b1743
Peter-Massarello <pmassarello@gmail.com>
2021-12-10 15:33:22 -0600

    edit file

--- program.fs
+++ program.fs
@@ -1,12 +1,10 @@
 && p2g1, smallest program and a good idea to make variants of this with each statement &&
 declare x = 5 ;
-declare pp = 69 ;
 program
 start
     declare x = 1;
     declare y = 2;
     label y;
     talk x;
-    talk pp;
     jump y;
 stop
